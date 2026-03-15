8521adca60dde7b4ec7167d902b4c36c3ce4b84a
Bryan Oltman <bryanoltman@gmail.com>
2026-03-14 20:39:13 -0400

    Initial Commit

---
+++ ListenToWikipedia/ListenToWikipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListenToWikipediaApp.swift
+//  ListenToWikipedia
+//
+//  Created by Bryan Oltman on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct ListenToWikipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListenToWikipediaTests/ListenToWikipediaTests.swift
@@ -0,0 +1,16 @@
+//
+//  ListenToWikipediaTests.swift
+//  ListenToWikipediaTests
+//
+//  Created by Bryan Oltman on 3/14/26.
+//
+
+import Testing
+
+struct ListenToWikipediaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
