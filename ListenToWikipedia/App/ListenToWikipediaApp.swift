0bf9f78525ad9ece7cd11439ab0595cf27e10187
Bryan Oltman <bryanoltman@gmail.com>
2026-03-20 07:53:28 -0400

    Don't let phone fall asleep if app is foregrounded

--- ListenToWikipedia/App/ListenToWikipediaApp.swift
+++ ListenToWikipedia/App/ListenToWikipediaApp.swift
@@ -2,11 +2,20 @@ import SwiftUI
 
 @main
 struct ListenToWikipediaApp: App {
+  #if os(iOS)
+  @Environment(\.scenePhase) private var scenePhase
+  #endif
+
   var body: some Scene {
     WindowGroup {
       ContentView()
         .environmentObject(AppSettings.shared)
     }
+    #if os(iOS)
+    .onChange(of: scenePhase) { _, phase in
+      UIApplication.shared.isIdleTimerDisabled = (phase == .active)
+    }
+    #endif
 
     #if os(macOS)
       Settings {
