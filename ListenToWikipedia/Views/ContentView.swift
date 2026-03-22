9120c0ebe70164b0439ffb408653b953c846a965
Bryan Oltman <bryanoltman@gmail.com>
2026-03-22 00:22:35 -0400

    Tweak settings button placement

--- ListenToWikipedia/Views/ContentView.swift
+++ ListenToWikipedia/Views/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
         }
         .buttonBorderShape(.circle)
         .buttonStyle(.glass)
-        .padding()
+        .padding([.leading])
       } else {
         Button(action: openSettingsAction) {
           Image(systemName: "gearshape.fill")
@@ -50,7 +50,7 @@ struct ContentView: View {
         }
         .buttonBorderShape(.circle)
         .buttonStyle(.plain)
-        .padding()
+        .padding([.leading])
       }
     }
     .overlay(alignment: .bottom) {
