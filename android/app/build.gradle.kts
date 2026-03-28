7759cea54c0947fa683d222582bf8f34fbcd5bfb
Bryan Oltman <bryanoltman@gmail.com>
2026-03-28 14:12:02 -0400

    upgrade libs to satisfy 16KB alignment requirement

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.mididriver)
+    implementation(files("libs/MidiDriver-1.29.aar"))
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
