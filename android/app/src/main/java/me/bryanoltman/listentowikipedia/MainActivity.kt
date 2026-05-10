eaad8127746f9c25c7410c1a007f99a88da85af2
Bryan Oltman <bryanoltman@gmail.com>
2026-05-09 22:45:57 -0400

    [android] set edge-to-edge status bar style to transparent to allow for white status bar text

--- android/app/src/main/java/me/bryanoltman/listentowikipedia/MainActivity.kt
+++ android/app/src/main/java/me/bryanoltman/listentowikipedia/MainActivity.kt
@@ -3,6 +3,7 @@ package me.bryanoltman.listentowikipedia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import me.bryanoltman.listentowikipedia.model.AppSettings
 import me.bryanoltman.listentowikipedia.ui.ContentScreen
@@ -11,7 +12,9 @@ import me.bryanoltman.listentowikipedia.ui.theme.ListenToWikipediaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT),
+        )
         val settings = AppSettings.getInstance(applicationContext)
         setContent {
             ListenToWikipediaTheme {
